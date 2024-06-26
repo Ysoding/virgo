82bbc2fe8715ce7f9eaf83d71dc1790ed12a78aa
ultraman <1394466835@qq.com>
2024-06-26 20:48:15 +0800

    feat: say hi

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	tele "gopkg.in/telebot.v3"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	pref := tele.Settings{
+		Token:  os.Getenv("TOKEN"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hi", func(ctx tele.Context) error {
+		return ctx.Send("你好! 我是张无忌")
+	})
+
+	b.Start()
+
+}
