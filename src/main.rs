563463540563b694b94280a4e9df7121e5951d32
ultraman <1394466835@qq.com>
2025-01-04 13:13:53 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::error::Error;
+
 use anyhow::Result;
+use atom_syndication::Feed;
 use dotenv::dotenv;
 use teloxide::{prelude::*, utils::command::BotCommands};
 use tokio_postgres::NoTls;
@@ -11,7 +14,7 @@ async fn main() -> Result<()> {
 
     let bot = Bot::from_env();
 
-    connect_postgres().await?;
+    // connect_postgres().await?;
 
     Command::repl(bot, answer).await;
     Ok(())
@@ -38,8 +41,42 @@ async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
                 .await?
         }
         Command::Hi => bot.send_message(msg.chat.id, "你好! 我是张无忌").await?,
-        Command::Rss => bot.send_message(msg.chat.id, "你好! 我是张无忌").await?,
+        Command::Rss => {
+            if let Err(e) = update_youtube_channel_rss(&bot, &msg, "UCrqM0Ym_NbK1fqeQG2VIohg").await
+            {
+                log::error!("{:?}", e);
+            }
+            bot.send_message(msg.chat.id, "你好! 我是张无忌").await?
+        }
     };
+
+    Ok(())
+}
+
+async fn update_youtube_channel_rss(
+    bot: &Bot,
+    msg: &Message,
+    channel_id: &str,
+) -> Result<(), Box<dyn Error>> {
+    let url = format!("https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}");
+    let content = reqwest::get(&url).await?.text().await?;
+    log::info!("Fetched RSS feed from: {}", url);
+
+    let feed = content.parse::<Feed>()?;
+    log::info!("Parsed Atom feed successfully");
+
+    let title = feed.title.value;
+    let link = &feed.links[0].href;
+    log::info!("Channel Title: {}", title);
+    log::info!("Channel Link: {}", link);
+
+    if let Some(entry) = feed.entries.first() {
+        log::info!("\nVideo Title: {}", entry.title.value);
+        log::info!("Video Link: {}", entry.links[0].href);
+        log::info!("Published Date: {}", entry.published.unwrap_or_default());
+        let message = format!("{}\n{}", title, entry.links[0].href);
+        bot.send_message(msg.chat.id, message).await?;
+    }
 
     Ok(())
 }
