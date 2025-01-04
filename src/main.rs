8a09d3048c08db8798f010c8caf21be53b1682eb
ultraman <1394466835@qq.com>
2025-01-04 09:28:27 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
+use anyhow::Result;
 use dotenv::dotenv;
 use teloxide::{prelude::*, utils::command::BotCommands};
+use tokio_postgres::NoTls;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     pretty_env_logger::init();
     dotenv().ok();
     log::info!("Starting command bot...");
 
     let bot = Bot::from_env();
 
+    connect_postgres().await?;
+
     Command::repl(bot, answer).await;
+    Ok(())
 }
 
 #[derive(BotCommands, Clone)]
@@ -22,6 +27,8 @@ enum Command {
     Help,
     #[command(description = "who are you?")]
     Hi,
+    #[command(description = "Push some info")]
+    Rss,
 }
 
 async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
@@ -31,7 +38,33 @@ async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
                 .await?
         }
         Command::Hi => bot.send_message(msg.chat.id, "你好! 我是张无忌").await?,
+        Command::Rss => bot.send_message(msg.chat.id, "你好! 我是张无忌").await?,
     };
 
+    Ok(())
+}
+
+async fn connect_postgres() -> Result<()> {
+    // Connect to the database.
+    let (client, connection) = tokio_postgres::connect(
+        "host=localhost user=postgres password=password connect_timeout=10",
+        NoTls,
+    )
+    .await?;
+
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    // Now we can execute a simple statement that just returns its parameter.
+    let rows = client.query("SELECT $1::TEXT", &[&"hello world"]).await?;
+
+    // And then check that we got back the same string we sent over.
+    let value: &str = rows[0].get(0);
+    println!("{}", value);
     Ok(())
 }
